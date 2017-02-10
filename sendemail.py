19a9b13093086c6bf6bb128cb86fb019fc3f8ce0
BacchasSeaford <richybacchas0911@gmail.com>
2017-02-10 18:41:23 -0500

    Email

---
+++ sendemail.py
@@ -0,0 +1,23 @@
+import smtplib
+from_addr = ''
+to_addr = ''
+message = """From: {} <{}>
+To: {} <{}> 
+Subject: {}
+{} 
+"""
+from_name = 'Ricky'
+to_name = 'Seaford'
+subject = 'Lab three'
+msg =' This is where my life belong! '
+message_to_send = message.format(from_name, from_addr, to_name,
+ to_addr, subject, msg)
+# Credentials (if needed)
+username = ''
+password = ''
+# The actual mail send
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.starttls()
+server.login(username, password)
+server.sendmail(from_addr, to_addr, message_to_send)
+server.quit()
